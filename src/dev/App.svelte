66ce3cf17ce13cb4e4f49a92f277288dc1a3df42
André Lins <andrelucas01@hotmail.com>
2020-05-28 23:10:37 -0300

    First commit

---
+++ src/dev/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import DeviceDetector from "../lib/DeviceDetector.svelte";
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <h1>Svelte Device Detector</h1>
+  <DeviceDetector />
+</main>
---
+++ src/lib/DeviceDetector.svelte
@@ -0,0 +1,15 @@
+<script>
+  import UAParser from "ua-parser-js";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    console.log("ENTROU");
+    const uaParser = new UAParser();
+    console.log(uaParser);
+    console.log(uaParser.getBrowser());
+    console.log(uaParser.getDevice());
+    console.log(uaParser.getOS());
+  });
+</script>
+
+<p>Teste</p>
